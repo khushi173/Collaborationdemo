5cc3595a3c08ed29f1c041dae1cce83c1447a3d1
khushi173 <khushijamwal173@gmail.com>
2022-03-17 17:41:15 +0530

    updated

--- src/com/harman/calculator/Mainfile.java
+++ src/com/harman/calculator/Mainfile.java
@@ -4,11 +4,14 @@ public class Mainfile {
     public static void main(String[] args) {
         Addition ob_add = new Addition();
         Division1 ob_div = new Division1();
-        int x =10 ,y = 20 , result1, result2;
+        Substraction ob_sub = new Substraction();
+        int x =10 ,y = 20 , result1, result2, result3;
         result1 = ob_add.add(x,y);
         result2 = ob_div.div(x,y);
+        result3 = ob_sub.sub(x,y);
         System.out.println(result1);
         System.out.println(result2);
+        System.out.println(result3);
 
     }
 }
